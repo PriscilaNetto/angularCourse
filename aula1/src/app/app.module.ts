63df05998c995a9d34593da3b6025566985b00d5
PriscilaNetto <priscila.nnetto@hotmail>
2021-06-22 17:19:57 -0300

    add first and second component

---
+++ aula1/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+//coloquei o first component abaixo em declarations para aparecer na tela e precisa fazer a importação acima
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
