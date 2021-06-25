bd0efee4782bdbb37c6e411cab18e50ab84d9162
PriscilaNetto <priscila.nnetto@hotmail>
2021-06-25 18:42:45 -0300

    acesso ao DOM, template com viewChild

--- aula3-data-binding/src/app/output-property/output-property.component.ts
+++ aula3-data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -8,17 +8,20 @@ import { Component, Input, OnInit, EventEmitter, Output} from '@angular/core';
 })
 export class OutputPropertyComponent implements OnInit {
 
- @Input() valor: number = 0;
- @Output() mudouValor = new EventEmitter();
+  @Input() valor: number = 0;
+  @Output() mudouValor = new EventEmitter();
+
+  @ViewChild('campoInput')
+  campoValorInput!: ElementRef;
 
   incrementa() {
-    this.valor++
-    this.mudouValor.emit({novoValor: this.valor});
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa() {
-    this.valor--
-    this.mudouValor.emit({novoValor: this.valor});
+    this.campoValorInput.nativeElement.value--;
+    this.mudouValor.emit({ novoValor: this.valor });
   }
   constructor() { }
 
